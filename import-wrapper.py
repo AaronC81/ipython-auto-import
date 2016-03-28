9e70ef87daac46f6e1a70bfc59b0e0da904ddbbd
OrangeFlash81 <aaronc20000@gmail.com>
2016-03-28 15:58:34 +0100

    Minor formatting change and PEP8

--- import-wrapper.py
+++ import-wrapper.py
@@ -1,12 +1,17 @@
-import sys, IPython, colorama
+import sys
+import IPython
+import colorama
+import re
 
 colorama.init()
 
+
 def custom_exc(shell, etype, evalue, tb, tb_offset=None):
-    pre = colorama.Fore.CYAN + colorama.Style.BRIGHT + "AutoImport: " + colorama.Style.NORMAL + colorama.Fore.WHITE
+    pre = (colorama.Fore.CYAN + colorama.Style.BRIGHT +
+           "AutoImport: " + colorama.Style.NORMAL +
+           colorama.Fore.WHITE)
     if etype == NameError:
-        shell.showtraceback((etype, evalue, tb), tb_offset) # Show the normal traceback
-        import re
+        shell.showtraceback((etype, evalue, tb), tb_offset)
         try:
             # Get the name of the module you tried to import
             results = re.match("name '(.*)' is not defined", str(evalue))
@@ -20,20 +25,23 @@ def custom_exc(shell, etype, evalue, tb, tb_offset=None):
 
             # Import the module
             IPython.get_ipython().run_code("import {}".format(name))
-            print(pre + "Imported referenced module \"{}\", will retry".format(name))
+            print(pre +
+                  "Imported referenced module \"{}\", will retry".format(name))
+            print(colorama.Fore.CYAN + "".join("-" for _ in range(75)))
         except Exception as e:
-            print(pre + "Attempted to import \"{}\" but an exception occured".format(name))
+            print(pre + ("Attempted to import \"{}\""
+                  "but an exception occured".format(name)))
 
         try:
             # Run the failed line again
-            res = IPython.get_ipython().run_cell(list(get_ipython().history_manager.get_range())[-1][-1])
+            p = IPython.get_ipython()
+            res = p.run_cell(list(p.history_manager.get_range())[-1][-1])
         except Exception as e:
             print(pre + "Another exception occured while retrying")
             shell.showtraceback((type(e), e, None), None)
     else:
         shell.showtraceback((etype, evalue, tb), tb_offset=tb_offset)
 
-    
 
 # Bind the function we created to IPython's exception handler
 IPython.get_ipython().set_custom_exc((Exception,), custom_exc)
